0dbbf714aa1a37df9c6ce41f5a28e02b78631132
mstarner <mstarner8@gmail.com>
2017-11-29 08:35:02 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        public class Tester
+        {
+            public static void main(String args[]) {
+                String t1 = cat(68, 108);
+                System.out.println(t1);
+
+                String t2 = cat(35, 59);
+                System.out.println(t2);
+            }
+
+            //not sure how to do this project. Don't understand how StringBuffer works in this.
+
+        }
+    }
+}
